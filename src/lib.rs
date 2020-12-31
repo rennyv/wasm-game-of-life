3f767948594bf431327bd4ea94c58528a627fd51
renny <renny@vanneste.ca>
2020-12-31 15:51:42 -0700

    Updated README

--- src/lib.rs
+++ src/lib.rs
@@ -104,7 +104,7 @@ impl Universe {
 
         let cells = (0..width * height)
             .map(|i| {
-                if i % 2 == 0 || i % 7 == 0 {
+                if i % 2 == 0 || i % 31 == 0 {
                     Cell::Alive
                 } else {
                     Cell::Dead
