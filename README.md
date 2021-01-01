366e6d423bf4a16138fe0b46d8fc9b7c0cb05316
renny <renny@vanneste.ca>
2020-12-31 19:29:59 -0700

    Added a simple test

--- README.md
+++ README.md
@@ -10,9 +10,14 @@ Simple demo of wasm in Rust creating the Conway's Game of Life
   - [Follow instructions here](https://www.npmjs.com/get-npm)
 - Generated with cargo-generate
   - `cargo install cargo-generate`
+- Testing you need firefox installed
+  - (Ubuntu) `sudo apt install firefox`
 ## Setup
 
 1. `wasm-pack build`
 2. `cd www`
 3. `npm install`
 4. `npm run start`
+
+## Testing
+1. `wasm-pack test --firefox --headless`
--- src/lib.rs
+++ src/lib.rs
@@ -40,6 +40,16 @@ impl fmt::Display for Universe {
 }
 
 impl Universe {
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        self.cells = (0..width * self.height).map(|_i| Cell::Dead).collect();
+    }
+
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        self.cells = (0..self.width * height).map(|_i| Cell::Dead).collect();
+    }
+
     fn get_index(&self, row: u32, column: u32) -> usize {
         (row * self.width + column) as usize
     }
@@ -60,6 +70,22 @@ impl Universe {
         }
         count
     }
+}
+impl Universe {
+    /// Get the dead and alive values of the entire universe.
+    pub fn get_cells(&self) -> &[Cell] {
+        &self.cells
+    }
+
+    /// Set cells to be alive in a universe by passing the row and column
+    /// of each cell as an array.
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells[idx] = Cell::Alive;
+        }
+    }
+
 }
 
 /// Public methods, exported to JavaScript.
